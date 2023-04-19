b15cca7551ed04518a7d16f2e8d763abe5e0f7e7
MoonJihye <jihyer0395@gmail.com>
2023-04-19 23:12:32 +0900

    Spring-React 연결

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
+
+  const [message, setMessage]=useState([]);
+
+  useEffect(()=>{
+    fetch("/api/demo-web")
+        .then((response)=>{
+          return response.json();
+        })
+        .then((data)=>{
+          setMessage(data);
+        });
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {message}
         </p>
         <a
           className="App-link"
